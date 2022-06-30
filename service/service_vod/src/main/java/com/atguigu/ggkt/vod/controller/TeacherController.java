4d26c28d760bf8a95f8b9d337e420aa9415188cf
qxfexbea <qinxianfu123@163.com>
2022-06-30 20:42:32 +0800

    第一天记录teacherController

--- service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
+++ service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
@@ -2,8 +2,17 @@ package com.atguigu.ggkt.vod.controller;
 
 
 import com.atguigu.ggkt.model.vod.Teacher;
+import com.atguigu.ggkt.result.Result;
+import com.atguigu.ggkt.vo.vod.TeacherQueryVo;
 import com.atguigu.ggkt.vod.service.TeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,29 +25,102 @@ import java.util.List;
  * @author atguigu
  * @since 2022-06-30
  */
+@Api(tags = "讲师管理接口")
 @RestController
-@RequestMapping("/admin/vod/teacher")
+@RequestMapping(value="/admin/vod/teacher")
 public class TeacherController {
-    @Autowired
 
+    @Autowired
     private TeacherService teacherService;
 
+    // http://localhost:8301/admin/vod/teacher/findAll
+    //1 查询所有讲师
+    @ApiOperation("查询所有讲师")
+    @GetMapping("findAll")
+    public Result findAllTeacher() {
+        //调用service方法
+        List<Teacher> list = teacherService.list();
+        return Result.ok(list).message("查询数据成功");
+    }
+
+    // remove/1
+    //2 逻辑删除讲师
+    @ApiOperation("逻辑删除讲师")
+    @DeleteMapping("remove/{id}")
+    public Result removeTeacher(@ApiParam(name = "id", value = "ID", required = true)
+                                @PathVariable Long id) {
+        boolean isSuccess = teacherService.removeById(id);
+        if(isSuccess) {
+            return Result.ok(null);
+        } else {
+            return Result.fail(null);
+        }
+    }
+    //条件查询分页列表
+    @ApiOperation(value = "获取分页列表")
+    @PostMapping("findQueryPage/{page}/{limit}")
+    public Result index(
+            @ApiParam(name = "page", value = "当前页码", required = true) @PathVariable Long page,
+            @ApiParam(name = "limit", value = "每页记录数", required = true) @PathVariable Long limit,
+            @ApiParam(name = "teacherVo", value = "查询对象", required = false) @RequestBody(required = false) TeacherQueryVo teacherQueryVo) {
+        //创建page对象，传递当前页和每页记录数
+        Page<Teacher> pageParam = new Page<>(page, limit);
+        //获取条件值
+        String name = teacherQueryVo.getName();//讲师名称
+        Integer level = teacherQueryVo.getLevel();//讲师级别
+        String joinDateBegin = teacherQueryVo.getJoinDateBegin();//开始时间
+        String joinDateEnd = teacherQueryVo.getJoinDateEnd();//结束时间
+        //封装条件
+        QueryWrapper<Teacher> wrapper = new QueryWrapper<>();
+        if(!StringUtils.isEmpty(name)) {
+            wrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(level)) {
+            wrapper.eq("level",level);
+        }
+        if(!StringUtils.isEmpty(joinDateBegin)) {
+            wrapper.ge("join_date",joinDateBegin);
+        }
+        if(!StringUtils.isEmpty(joinDateEnd)) {
+            wrapper.le("join_date",joinDateEnd);
+        }
+        //调用方法得到分页查询结果
+        IPage<Teacher> pageModel = teacherService.page(pageParam, wrapper);
+        return Result.ok(pageModel);
+    }
+
+    @ApiOperation(value = "添加讲师")
+    @PostMapping("/saveTeacher")
+    public Result saveTeacher(@RequestBody Teacher teacher){
+        boolean isSuccess = teacherService.save(teacher);
+        if(isSuccess) {
+            return Result.ok(null);
+        } else {
+            return Result.fail(null);
+        }
+    }
+
     /**
-     * 查询所有讲师列表
+     * 通过id查询用户
+     * @param id
      * @return
      */
-    @GetMapping("findAll")
-    public List<Teacher> findAll(){
-        List<Teacher> list = teacherService.list();
-        return list;
+    @ApiOperation("id查询接口")
+    @GetMapping("getTeacher/{id}")
+    public Result getTeacher(@PathVariable("id")Long id){
+        Teacher teacher = teacherService.getById(id);
+        return Result.ok(teacher);
+
     }
-    /**删除讲师
-     *
-     */
-    @DeleteMapping("remove/{id}")
-    public boolean removeById(@PathVariable String id){
-        System.out.println("ajjajaja");
-        return teacherService.removeById(id);
+    @ApiOperation("修改最终实现")
+    @PostMapping("updateTeacher")
+    public Result updateTeacher(@RequestBody Teacher teacher){
+        boolean isSuccess = teacherService.updateById(teacher);
+        if(isSuccess) {
+            return Result.ok(null);
+        } else {
+            return Result.fail(null);
+        }
     }
 }
 
